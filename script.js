857da7b5b985cf455f73f3749f42156fd7128ae7
Kevin Baltazar Reyes <21996242+kay-rey@users.noreply.github.com>
2025-07-12 01:09:48 -0700

    initial commit with working site

---
+++ script.js
@@ -0,0 +1,7 @@
+// Initialize the AOS (Animate on Scroll) library
+AOS.init({
+    duration: 1000, // Animation duration in milliseconds
+    once: true,     // Whether animation should happen only once - while scrolling down
+    offset: 100,    // Offset (in px) from the original trigger point
+    easing: 'ease-in-out', // Default easing for AOS animations
+});
